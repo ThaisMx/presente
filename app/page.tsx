fd1fae30a2da2e5ed73607d4d23a70fcbb22cc9d
Thais Maximiana <thaismaximiana@Mac-mini-de-Thais.local>
2025-04-01 17:05:45 -0300

    Corrigindo referência da imagem para ezequiel.jpg

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="relative h-[33vh] bg-[#D35400] overflow-hidden">
         <div className="absolute inset-0">
           <img
-            src="https://raw.githubusercontent.com/ThaisMx/presente/refs/heads/main/public/will.jpg"
+            src="https://raw.githubusercontent.com/ThaisMx/presente/refs/heads/main/public/ezequiel.jpg"
             alt="Will com café Starbucks"
             className="w-full h-full object-cover opacity-40"
           />
